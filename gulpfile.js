07453fd954a2ea14d7185b7c9310f16977fdfda4
todddrinkwater <todddrinkwaternz@gmail.com>
2017-03-27 21:34:47 +1300

    setup del tutorial stuff

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,15 @@ var del = require('del')
 gulp.task('todd', function() {
   console.log("!!!");
 });
+
+gulp.task('clean:mobile', function () {
+  return del([
+    'client/report.csv',
+    // here we use a globbing pattern to match everything inside the `mobile` folder
+    'client/mobile/**/*',
+    // we don't want to clean this file though so we negate the pattern
+    '!client/mobile/deploy.json'
+  ]);
+});
+
+gulp.task('default', ['todd', 'clean:mobile']);
